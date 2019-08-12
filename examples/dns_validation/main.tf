1f4dd2349cd8cfe5539afff904663245d477901b
Rob Russo <robert.russo@hbo.com>
2019-08-12 13:23:03 -0400

    remove extaneous files

--- examples/dns_validation/main.tf
+++
@@ -1,9 +0,0 @@
-module "acm" {
-  source = "../../"
-  domain_name      = "*.stg-tvlk.cloud"
-  hosted_zone_name = "stg-tvlk.cloud"
-  certificate_name = "wildcard.stg-tvlk.cloud"
-  environment      = "staging"
-  description      = "Wildcard certificate for example.stg-tvlk.cloud"
-  product_domain   = "exm"
-}
