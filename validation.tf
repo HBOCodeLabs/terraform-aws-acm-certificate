49a55946063e936322769d569daa22bca979929c
Joshua Evans <joshua.evans@hbo.com>
2020-10-09 23:11:01 -0400

    erase bracket around func

--- validation.tf
+++ validation.tf
@@ -1,14 +1,14 @@
 resource "aws_acm_certificate_validation" "dns_validation" {
   certificate_arn         = aws_acm_certificate.this.arn
 
-  validation_record_fqdns = "${concat(
+  validation_record_fqdns = concat(
     list(aws_route53_record.core_zone.fqdn),
     aws_route53_record.mgmt_zones.*.fqdn,
     aws_route53_record.nonprod_zones.*.fqdn,
     aws_route53_record.sandbox_zones.*.fqdn,
     aws_route53_record.hbogo_zones.*.fqdn,
     aws_route53_record.production_zones.*.fqdn
-    )}"
+    )
 
   count = var.enable_validation ? 1 : 0
 }
