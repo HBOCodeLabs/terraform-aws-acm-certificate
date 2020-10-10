b9533389e4dd09e962459de92fb4687158d2c7ce
Joshua Evans <joshua.evans@hbo.com>
2020-10-09 22:32:35 -0400

    de-bracket a function

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ resource "aws_acm_certificate" "this" {
   domain_name       = var.domain_name
   validation_method = "DNS"
 
-  subject_alternative_names = "${concat(
+  subject_alternative_names = concat(
     var.subject_alternative_names_mgmt,
     var.subject_alternative_names_nonprod,
     var.subject_alternative_names_hbogo,
     var.subject_alternative_names_sandbox,
     var.subject_alternative_names_production
-  )}"
+  )
 
   tags = {
     Name                      = var.certificate_name
